f186bedb0f32f80f286d0138d913b9c16c9aaade
Tim Gabrikowski <tim@gabrikowski.de>
2023-06-17 15:35:24 +0200

    delete old stuff

--- src/inputTypes.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	VIDEO_ID: "video id",
-	PLAYLIST_ID: "playlist id",
-	VIDEO_LINK: "video link",
-	PLAYLIST_LINK: "playlist link",
-	MIXED_LINK: "mixed link",
-	SHORT_LINK: "short link",
-};
