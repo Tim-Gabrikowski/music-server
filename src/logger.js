d8083c11c0af5ed05cc70eb9a0bae6f241310aa5
Tim Gabrikowski <tim@gabrikowski.de>
2023-12-29 11:19:03 +0100

    fix logger

--- src/logger.js
+++ src/logger.js
@@ -36,9 +36,9 @@ function timeDateString() {
 	let date = ("0" + date_ob.getDate()).slice(-2);
 	let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 	let year = date_ob.getFullYear();
-	let hours = ("0" + (date_ob.getHours() + 1)).slice(-2);
-	let minutes = ("0" + (date_ob.getMinutes() + 1)).slice(-2);
-	let seconds = ("0" + (date_ob.getSeconds() + 1)).slice(-2);
+	let hours = ("0" + date_ob.getHours()).slice(-2);
+	let minutes = ("0" + date_ob.getMinutes()).slice(-2);
+	let seconds = ("0" + date_ob.getSeconds()).slice(-2);
 	return (
 		year +
 		"." +
